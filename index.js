114912a061a0a3287d79c427ab5c6a7ec6d43aae
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-06-03 00:14:54 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add (a , b){
+return a + b
+}
+function subtract(a , b) {
+return a - b
+}
+function multiply(a , b){
+return a * b
+}
+function divide(a , b){
+return a / b
+}
+n= 5
+function increment(n){
+  return ( n += 1)
+}
+console.log(increment(n))
+n= 5
+function decrement(n){
+  return ( n -= 1)
+}
+n=3
+function makeInt(n){
+    return n
+}
+
+function makeInt(n) { 
+    parseFloat("n", 10)
+return parseFloat(n)
+}
+n= 89.33
+function preserveDecimal (n){
+    parseFloat("n")
+    return parseFloat(n)
+}
