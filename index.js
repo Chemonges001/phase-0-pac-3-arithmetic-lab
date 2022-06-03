252f1a3300dd6f25c07a0170354a121f115b937c
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-06-03 17:23:25 +0300

    Done.

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function increment(n){
   return ( n += 1)
 }
 console.log(increment(n))
-n= 5
+
 function decrement(n){
   return ( n -= 1)
 }
-n=3
+
 function makeInt(n){
     return n
 }
@@ -28,7 +28,7 @@ function makeInt(n) {
     parseFloat("n", 10)
 return parseFloat(n)
 }
-n= 89.33
+
 function preserveDecimal (n){
     parseFloat("n")
     return parseFloat(n)
